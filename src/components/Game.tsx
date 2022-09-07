e48ef8b537f5829b727d90eac0e527426bf84bec
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-09-07 16:57:15 +0200

    game functionnalities

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,41 @@
-import React, { useContext } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect, useState } from 'react';
 
 import CurrentUserContext from '../contexts/CurrentUser';
 
 const Game = () => {
   const { name, avatar } = useContext(CurrentUserContext);
-  const randomNumber: number = Math.floor(Math.random() * 100) + 1;
-  const tries: Array<Array<number | string>> = [
-    [50, "C'est plus !"],
-    [80, "C'est moins !"],
-    [3, "C'est plus !"],
-  ];
+
+  const [randomNumber, setRandomNumber] = useState<number>(0);
+  useEffect(() => {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }, []);
+
+  const [tries, setTries] = useState<Array<Array<number | string>>>([]);
+  const [nbTries, setNbTries] = useState<number>(1);
+
+  const [userNumberStr, setUserNumberStr] = useState<string>('');
+
+  const [hint, setHint] = useState<string>('-');
+
+  const handleValidation = () => {
+    const userNumber = Number(userNumberStr);
+    setNbTries(nbTries + 1);
+    if (userNumber < randomNumber) {
+      setHint("C'est plus !");
+      tries.push([nbTries, userNumber, "C'est plus !"]);
+    }
+    if (userNumber > randomNumber) {
+      setHint("C'est moins !");
+      tries.push([nbTries, userNumber, "C'est moins !"]);
+    }
+    if (userNumber === randomNumber) {
+      setHint('Bravo !');
+      tries.push([nbTries, userNumber, 'Bravo !']);
+    }
+    console.log(tries);
+    setUserNumberStr('');
+  };
 
   return (
     <div className="game">
@@ -32,49 +58,92 @@ const Game = () => {
             {tries &&
               tries.map((oneTry, index) => (
                 <li key={index}>
-                  <span>{index + 1}</span>
                   <span>{oneTry[0]}</span>
                   <span>{oneTry[1]}</span>
+                  <span>{oneTry[2]}</span>
                 </li>
               ))}
           </ul>
         </div>
         <div className="game__core__userAction">
-          <p className="game__core__userAction__hint">C&apos;est plus !</p>
+          <p className="game__core__userAction__hint">{hint}</p>
           <div className="game__core__userAction__numberGrid">
-            <div className="game__core__userAction__numberGrid__input">in</div>
-            <div className="game__core__userAction__numberGrid__delete">x</div>
-            <div className="game__core__userAction__numberGrid__number1">
+            <div className="game__core__userAction__numberGrid__input">
+              {userNumberStr && userNumberStr}
+              <span>|</span>
+            </div>
+            <button
+              onClick={() => setUserNumberStr('')}
+              type="button"
+              className="game__core__userAction__numberGrid__delete">
+              <img src="./assets/numbers/delete.png" alt="delete" />
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '1')}
+              type="button"
+              className="game__core__userAction__numberGrid__number1">
               <img src="./assets/numbers/one.png" alt="one" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number2">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '2')}
+              type="button"
+              className="game__core__userAction__numberGrid__number2">
               <img src="./assets/numbers/two.png" alt="two" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number3">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '3')}
+              type="button"
+              className="game__core__userAction__numberGrid__number3">
               <img src="./assets/numbers/three.png" alt="three" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number4">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '4')}
+              type="button"
+              className="game__core__userAction__numberGrid__number4">
               <img src="./assets/numbers/four.png" alt="four" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number5">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '5')}
+              type="button"
+              className="game__core__userAction__numberGrid__number5">
               <img src="./assets/numbers/five.png" alt="five" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number6">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '6')}
+              type="button"
+              className="game__core__userAction__numberGrid__number6">
               <img src="./assets/numbers/six.png" alt="six" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number7">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '7')}
+              type="button"
+              className="game__core__userAction__numberGrid__number7">
               <img src="./assets/numbers/seven.png" alt="seven" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number8">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '8')}
+              type="button"
+              className="game__core__userAction__numberGrid__number8">
               <img src="./assets/numbers/eight.png" alt="eight" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number9">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '9')}
+              type="button"
+              className="game__core__userAction__numberGrid__number9">
               <img src="./assets/numbers/nine.png" alt="nine" />
-            </div>
-            <div className="game__core__userAction__numberGrid__number0">
+            </button>
+            <button
+              onClick={() => setUserNumberStr(userNumberStr + '0')}
+              type="button"
+              className="game__core__userAction__numberGrid__number0">
               <img src="./assets/numbers/zero.png" alt="zero" />
-            </div>
-            <div className="game__core__userAction__numberGrid__validate">Valider</div>
+            </button>
+            <button
+              onClick={handleValidation}
+              type="button"
+              className="game__core__userAction__numberGrid__validate">
+              Valider
+            </button>
           </div>
         </div>
       </div>
